41050cbba3d1a560168ce3ef52db55e14bb748e0
Quynh <101940232+kwspringkle@users.noreply.github.com>
2024-07-30 22:04:58 +0700

    fix posts error

--- Posts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name='posts'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('delete/<int:post_id>/', views.delete_post, name='delete_post'),
-]
