ab360cd287a830cbd9b330dae70965972a9f4647
Quynh <101940232+kwspringkle@users.noreply.github.com>
2024-07-31 16:11:10 +0700

    fix posts

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Post(models.Model):
@@ -5,6 +6,7 @@ class Post(models.Model):
     image = models.ImageField(upload_to='images/', blank=True, null=True)
     video = models.FileField(upload_to='videos/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.content[:50] if self.content else "Media Post"
